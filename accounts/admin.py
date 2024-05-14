2562715ca775f5a4061b79961d05d973f134586c
fatemeh <fatemehsoleymani90@yahoo.com>
2024-05-14 17:17:16 +0430

    customize-register-admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+CustomUser = get_user_model()
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    fields = ('email', 'username')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
