dd6938cf10baa16de4d1be6e690c58a007dc863b
fatemeh <fatemehsoleymani90@yahoo.com>
2024-06-10 18:23:44 +0430

    custom-permission-and-test

--- books/tests.py
+++ books/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Permission
 from .models import Book, Review
 
 
@@ -11,6 +12,10 @@ class BookTest(TestCase):
             username='testuser',
             email='testuser@email.com',
             password='testpass123'
+        )
+
+        self.special_permission = Permission.objects.get(
+            codename='special_status'
         )
         self.book = Book.objects.create(
             title='Note Book',
@@ -29,13 +34,27 @@ class BookTest(TestCase):
         self.assertEqual(f'{self.book.author}', 'author1')
         self.assertEqual(f'{self.book.price}', '29.00')
 
-    def test_book_list_view(self):
+    def test_book_list_view_for_logged_in_user(self):
+        self.client.login(email='testuser@email.com', password='testpass123')
         response = self.client.get(reverse('book_list'))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, 'Note Book')
         self.assertTemplateUsed(response, 'books/book_list.html')
 
-    def test_book_detail_view(self):
+    def test_book_list_view_for_logged_out_user(self):  # new
+        self.client.logout()
+
+        response = self.client.get(reverse('book_list'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(
+            response, '%s?next=/books/' % (reverse('account_login')))
+        response = self.client.get(
+            '%s?next=/books/' % (reverse('account_login')))
+        self.assertContains(response, 'Log In')
+
+    def test_book_detail_view_with_permissions(self):
+        self.client.login(email='testuser@email.com', password='testpass123')
+        self.user.user_permissions.add(self.special_permission)
         response = self.client.get(self.book.get_absolute_url())
         no_response = self.client.get('books/123/')
         self.assertEqual(response.status_code, 200)
