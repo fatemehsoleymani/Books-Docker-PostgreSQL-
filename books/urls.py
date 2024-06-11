17bdd1e4044847f4dee6ea5b6d47b6a9e0ea89dd
fatemeh <fatemehsoleymani90@yahoo.com>
2024-06-11 11:28:04 +0430

    add-search-results

--- books/urls.py
+++ books/urls.py
@@ -1,8 +1,9 @@
-from .views import BookList, BookDetailView
+from .views import BookList, BookDetailView, SearchResultsListView
 from django.urls import path
 
 
 urlpatterns = [
     path('', BookList.as_view(), name='book_list'),
     path('<uuid:pk>/', BookDetailView.as_view(), name='book_detail'),
+    path('search/', SearchResultsListView.as_view(), name='search_results')
 ]
